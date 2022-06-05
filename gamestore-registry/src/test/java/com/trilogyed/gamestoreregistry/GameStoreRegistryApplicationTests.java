4070ea5fdc52389cbfc76acd4e427947657e53b2
NukaGrizz <nukagrizz@gmail.com>
2022-06-05 10:11:58 -0400

    Initial microservice architecture with Eureka Registry and Config Server

---
+++ gamestore-registry/src/test/java/com/trilogyed/gamestoreregistry/GameStoreRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.gamestoreregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
